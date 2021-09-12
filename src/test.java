15eb08863078c31f51ef71356220c7b3a75ced2b
sreedharkurra <kurra.sreedhar84@gmail.com>
2021-09-12 17:40:50 -0500

    my test file

---
+++ src/test.java
@@ -0,0 +1,34 @@
+package testngtutorial;
+import org.testng.annotations.Test;
+
+public class TestNgtutorial {
+	
+	@Test
+	
+	public void tc1()
+	{
+		
+		
+		System.out.println("Aparna");
+		
+	}
+	
+	@Test
+	
+	public void tc2()
+	{
+		
+		System.out.println("Kurra");
+	}
+	
+@Test
+
+public void tc3()
+	
+	{
+		System.out.println("Welcomeworld");
+	}
+	
+	}
+
+
